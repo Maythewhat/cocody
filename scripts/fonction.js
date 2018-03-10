cefee3ebc88005998155adcb7c618756ee3d50d4
Seloris <daniel.djordjevic@viacesi.fr>
2018-03-10 19:58:35 +0100

    en cours

--- scripts/fonction.js
+++ scripts/fonction.js
@@ -1,13 +1,17 @@
 // Document ready
 $(document).ready(function () {
-	var $total = $("#total");
-	var $chiffreA = $("#chiffreA");
-	var $chiffreB = $("#chiffreB");
-	$("input").keyup(function () {
-var total = +$chiffreA.val() + +$chiffreB.val();
-$total.text(total);
+    var $total = $("#total");
+    var $chiffreA = $("#chiffreA");
+    var $chiffreB = $("#chiffreB");
 
-	});
-
-
+    // A chaque touche du clavier tapée
+    $("input").keyup(function () {
+        // le + sert à convertir en chiffre
+        var total = +$chiffreA.val() + +$chiffreB.val();
+        if (isNaN(total)) {
+            $total.text('Il faut saisir des numéros pour a et b !')
+        } else {
+            $total.text(total);
+        }
+    });
 });
